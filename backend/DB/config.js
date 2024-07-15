03c8cb2bf960c070c3356ab17b60bcb3255f3735
TAMANNA <tamannakarthol320@gmail.com>
2024-07-14 23:28:34 -0700

    add

--- backend/DB/config.js
+++ backend/DB/config.js
@@ -1,3 +1,3 @@
 const mongoose =require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/FinalDB") 
+mongoose.connect("mongodb+srv://tamannakarthol320:tamanna2003@database.65llh4w.mongodb.net/") 
